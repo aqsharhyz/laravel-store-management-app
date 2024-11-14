cf140326c8081f6a703c3497e52c10f0ff9dc55b
Aqsha Rheyznandya Malik <aqsharheyznandyamalik@mail.ugm.ac.id>
2024-11-14 23:17:25 +0300

    Add Shipper resource with CRUD functionality and related pages

---
+++ app/Filament/Resources/ShipperResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ShipperResource\Pages;
+use App\Filament\Resources\ShipperResource\RelationManagers;
+use App\Models\Shipper;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ShipperResource extends Resource
+{
+    protected static ?string $model = Shipper::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-paper-airplane';
+
+    protected static ?string $navigationLabel = 'Shipper';
+
+    protected static ?string $modelLabel = 'Shipper';
+
+    protected static ?string $navigationGroup = 'Order Management';
+
+    protected static ?int $navigationSort = 5;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListShippers::route('/'),
+            'create' => Pages\CreateShipper::route('/create'),
+            'edit' => Pages\EditShipper::route('/{record}/edit'),
+        ];
+    }
+}
