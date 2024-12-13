242e380bd9d0c4a20cbcb52496d0acf71c6f843e
Aqsha Rheyznandya Malik <aqsharheyznandyamalik@mail.ugm.ac.id>
2024-12-13 23:09:24 +0300

    Update routes for web

--- routes/api.php
+++ routes/api.php
@@ -8,21 +8,6 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ShipperController;
 use App\Http\Controllers\SupplierController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-// Route::get('categories', [CategoryController::class, 'index']);
-// Route::post('categories', [CategoryController::class, 'store']);
-// Route::get('categories/{category}', [CategoryController::class, 'show']);
-// Route::patch('categories/{category}', [CategoryController::class, 'update']);
-// Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
-
-Route::resource('categories', CategoryController::class);
-Route::resource('suppliers', SupplierController::class);
-Route::resource('products', ProductController::class);
-Route::resource('shippers', ShipperController::class);
-Route::resource('orders', OrderController::class);
-
-Route::get('categories/{category}/products', [CategoryController::class, 'showProducts']);
-Route::get('suppliers/{supplier}/products', [SupplierController::class, 'showProducts']);
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\WishlistController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('products', ProductController::class)->only(['index', 'show']);
+Route::get('categories/{category}/products', [ProductController::class, 'showProductsByCategory']);
+Route::get('tags/{tag}/products', [ProductController::class, 'showProductsByTag']);
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('wishlists', WishlistController::class)->only(['index', 'store', 'destroy']);
+
+    Route::get('cart', [ProductController::class, 'showCart']);
+    Route::post('/products/{product}/cart', [CartController::class, 'store']);
+    Route::patch('cart/{product}', [CartController::class, 'update']);
+    Route::delete('cart/{product}', [CartController::class, 'destroy']);
+    Route::post('cart/checkout', [CartController::class, 'checkout']);
+
+    Route::resource('orders', OrderController::class)->only(['index', 'show', 'store']);
+    Route::get('orders/{order}/payment', [OrderController::class, 'showPayment']);
+    Route::get('orders/{order}/shipment', [OrderController::class, 'showShipment']);
+});
+
+// Route::get('suppliers/{supplier}/products', [SupplierController::class, 'showProducts']);
